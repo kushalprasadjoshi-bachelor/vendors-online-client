ebb898f7ed3b53c39af36c9fae0a5bdace4f3294
Kushal Prasad Joshi <kushalprasadjoshi@gmail.com>
2026-07-08 16:30:46 +0545

    chore: general maintainance and improvements
    - Added dependencies for bootstrap and react-bootstrap
    - Added favicon icon and logo
    - Cleaned up the garbage files

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+// Mantine
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css';
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
