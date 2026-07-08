28796ea7ca03014366ecdb0bebb1b9dbeeff8451
Alok jha <jha61aalok@gmail.com>
2026-07-08 14:04:08 +0545

    feat(ui): navbar

---
+++ src/components/Herosection.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Herosection = () => {
+  return (
+    <div className="hero-section">
+    <div className="hero-content">
+        <h1>
+            FIND YOUR BEST <br/>
+            MATCHES FROM <br/>
+        </h1>
+        <p>
+            Browse through diverse range of vendors connected with us and their<br/>
+            products,designed to bring out your daliy needs down to your doorstep<br/>
+            just in a single click
+        </p>
+        <div className="hero-buttons">
+            <buttons className="primary-btn">Shop Now</buttons>
+            
+
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Herosection
